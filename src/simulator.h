038ec0b3de31fc2e74afb6b3cfb66b038a936309
Dillon Huff <dillonhuff@gmail.com>
2018-08-23 15:49:43 -0700

    Creating real example

--- src/simulator.h
+++ src/simulator.h
@@ -6,10 +6,11 @@ namespace FuncGen {
 
   class Simulator {
 
+    const Function& f;
     std::map<std::string, BitVector> variableValues;
     
   public:
-    Simulator(const Function& f) {
+    Simulator(const Function& f_) : f(f_) {
       for (auto& v : f.outputMap()) {
         variableValues.insert({v.first, unknown_bv(v.second->bitWidth())});
       }
@@ -20,10 +21,17 @@ namespace FuncGen {
     }
 
     void setInput(const std::string& inputName, const BitVector& value) {
+      variableValues[inputName] = value;
+    }
+
+    void evaluateStatement(const Statement& stmt) {
       
     }
 
     void evaluate() {
+      for (auto stmt : f.getStatements()) {
+        evaluateStatement(*stmt);
+      }
     }
   };
 }
