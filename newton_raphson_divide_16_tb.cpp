03c8089983e00dc3e9365b36d31a51446b316e9b
Dillon Huff <dillonhuff@gmail.com>
2018-09-01 16:47:35 -0700

    Can now generate top module declaration

--- newton_raphson_divide_16_tb.cpp
+++ newton_raphson_divide_16_tb.cpp
@@ -2,4 +2,11 @@
 #include "verilated.h"
 
 int main() {
+  Vnewton_raphson_divide_16 top;
+  top.N = 8;
+  top.D = 2;
+
+  top.eval();
+
+  assert(top.Q == (8 / 2));
 }
--- src/synthesis.cpp
+++ src/synthesis.cpp
@@ -6,9 +6,36 @@ using namespace std;
 
 namespace FuncGen {
 
+  vector<Function*> neededFunctions(Function* f) {
+    return {f};
+  }
+  
   void synthesizeVerilog(Function* f, const SynthesisConstraints& constraints) {
     ofstream out(f->getName() + ".v");
-    out << "module " + f->getName() + "(); endmodule" << endl;
+
+    vector<Function*> nf =
+      neededFunctions(f);
+
+    for (auto f : nf) {
+      out << "module " + f->getName() + "(";
+      int i = 0;
+      int numPorts = f->inputMap().size() + f->outputMap().size();
+      for (auto in : f->inputMap()) {
+        out << "input [" << (in.second->bitWidth() - 1) << ":" << 0 << "] " << in.first << ", ";
+        i++;
+      }
+      for (auto outP : f->outputMap()) {
+        out << "output [" << (outP.second->bitWidth() - 1) << ":" << 0 << "] " << outP.first;
+        if (i < (numPorts - 1)) {
+          out << ", ";
+        }
+        i++;
+      }
+
+      out << "); endmodule" << endl;
+    }
+    
+
     out.close();
   }
 }
