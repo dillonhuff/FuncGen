2cbbbd00f45b122b620000bbfbc7a07ae734e8b9
Dillon Huff <dillonhuff@gmail.com>
2018-09-02 11:53:39 -0700

    Filling in synthesis code

--- src/synthesis.cpp
+++ src/synthesis.cpp
@@ -9,16 +9,41 @@ using namespace dbhc;
 
 namespace FuncGen {
 
+#define sc static_cast
+
+  typedef std::map<Value*, string> ValMap;
+
+  class CodeGenState {
+    unsigned uniqueNum;
+    
+  public:
+
+    ValMap valueNameMap;
+
+    CodeGenState() : uniqueNum(0) {}
+
+    std::string uniqueName() {
+      return uniqueName("synth_var");
+    }
+
+    std::string uniqueName(const std::string& prefix) {
+      auto num = uniqueNum;
+      uniqueNum++;
+      return prefix + "_" + to_string(num);
+    }
+
+  };
+
   std::string getWire(Value* expr,
-                      map<Value*, string>& valueNameMap,
+                      CodeGenState& state,
                       ostream& out) {
-    if (contains_key(expr, valueNameMap)) {
-      return map_find(expr, valueNameMap);
+    if (contains_key(expr, state.valueNameMap)) {
+      return map_find(expr, state.valueNameMap);
     } else {
       assert(expr->type() == EXPRESSION_TYPE_VARIABLE);
       auto var = static_cast<Variable*>(expr);
       out << "\twire [" + to_string(var->bitWidth() - 1) + ":0] " + var->getName() << ";" << endl;
-      valueNameMap.insert({var, var->getName()});
+      state.valueNameMap.insert({var, var->getName()});
       return var->getName();
     }
   }
@@ -49,20 +74,55 @@ namespace FuncGen {
     return needed;
   }
 
-#define sc static_cast
+  void assignVars(const std::string& lhs, const std::string& rhs, ostream& out) {
+    out << "\tassign " << lhs << " = " << rhs << ";" << endl;
+  }
 
-  typedef std::map<Value*, string> ValMap;
+  std::vector<std::string> portConnections(map<string, string>& conns) {
+    vector<string> strs;
+    for (auto conn : conns) {
+      strs.push_back("." + conn.first + "(" + conn.second + ")");
+    }
+    return strs;
+  }
 
+  std::string commaList(const std::vector<std::string>& strs) {
+    string str;
+    for (int i = 0; i < (int) strs.size(); i++) {
+      str += strs[i];
+      if (i < (((int) strs.size()) - 1)) {
+        str += ", ";
+      }
+    }
+    return str;
+  }
+  
   string generateExpression(Expression* expr,
-                            ValMap valueNameMap,
+                            CodeGenState& state,
                             ostream& out) {
     if (expr->type() == EXPRESSION_TYPE_VARIABLE) {
       auto var = sc<Variable*>(expr);
       return var->getName();
     } else if (expr->type() == EXPRESSION_TYPE_FUNCTION_CALL) {
       auto fc = sc<FunctionCall*>(expr);
-      out << "\t" << fc->functionName() << "();" << endl;
-      return fc->outputValueName();
+
+      map<string, string> inputMap;
+      for (auto in : fc->getInputs()) {
+        string name = state.uniqueName(fc->functionName() + "_" + in.first);
+        out << "\twire [" << (in.second->bitWidth() - 1) << ":0] " << name << ";" << endl;
+
+        string r = generateExpression(in.second, state, out);
+        assignVars(name, r, out);
+        inputMap.insert({in.first, name});
+      }
+
+      string outName = fc->outputValueName();
+      string outWire = state.uniqueName(outName);
+
+      inputMap.insert({outName, outWire});
+
+      out << "\t" << fc->functionName() << " " << state.uniqueName(fc->functionName()) << "(" << commaList(portConnections(inputMap)) << ");" << endl;
+      return outWire;
     } else if (expr->type() == EXPRESSION_TYPE_CONSTANT) {
       auto c = sc<ConstantValue*>(expr);
       auto bv = c->getValue();
@@ -72,11 +132,7 @@ namespace FuncGen {
     }
   }
 
-  void assignVars(const std::string& lhs, const std::string& rhs, ostream& out) {
-    out << "\tassign " << lhs << " = " << rhs << ";" << endl;
-  }
-
-  void generateStmt(map<Value*, string>& valueNameMap,
+  void generateStmt(CodeGenState& state,
                     Statement* stmt,
                     std::ostream& out) {
     if (stmt->type() == STATEMENT_TYPE_PRINT) {
@@ -88,8 +144,8 @@ namespace FuncGen {
 
       auto rhs = assign->getRHS();
 
-      string name = getWire(static_cast<Variable*>(lhs), valueNameMap, out);
-      string res = generateExpression(rhs, valueNameMap, out);
+      string name = getWire(static_cast<Variable*>(lhs), state, out);
+      string res = generateExpression(rhs, state, out);
       assignVars(name, res, out);
 
     } else {
@@ -103,7 +159,8 @@ namespace FuncGen {
     vector<Function*> nf =
       neededFunctions(f);
 
-    map<Value*, string> valueNameMap;
+    CodeGenState state;
+    //map<Value*, string> valueNameMap;
     
     for (auto f : nf) {
       out << "module " + f->getName() + "(";
@@ -111,7 +168,7 @@ namespace FuncGen {
       int numPorts = f->inputMap().size() + f->outputMap().size();
       for (auto in : f->inputMap()) {
         out << "input [" << (in.second->bitWidth() - 1) << ":" << 0 << "] " << in.first << ", ";
-        valueNameMap[in.second] = in.first;
+        state.valueNameMap[in.second] = in.first;
         i++;
       }
       for (auto outP : f->outputMap()) {
@@ -119,14 +176,14 @@ namespace FuncGen {
         if (i < (numPorts - 1)) {
           out << ", ";
         }
-        valueNameMap[outP.second] = outP.first;
+        state.valueNameMap[outP.second] = outP.first;
         i++;
       }
 
       out << ");" << endl;
 
       for (auto stmt : f->getStatements()) {
-        generateStmt(valueNameMap, stmt, out);
+        generateStmt(state, stmt, out);
         //out << "// " << stmt->toString(1) << endl;
       }
 
