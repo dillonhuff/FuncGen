7d8e67b24edd4d7a5c5bb8979d071ce397096595
Dillon Huff <dillonhuff@gmail.com>
2018-09-02 14:12:07 -0700

    Now have negative divide working?

--- newton_raphson_divide_16_tb.cpp
+++ newton_raphson_divide_16_tb.cpp
@@ -1,6 +1,10 @@
 #include "Vnewton_raphson_divide_16.h"
 #include "verilated.h"
 
+#include <iostream>
+
+using namespace std;
+
 int main() {
   Vnewton_raphson_divide_16 top;
   top.N = 8;
@@ -9,4 +13,15 @@ int main() {
   top.eval();
 
   assert(top.Q == (8 / 2));
+
+  top.N = -8;
+  top.D = 2;
+
+  top.eval();
+
+  cout << "-8 / 2 = " << (-8 / 2) << endl;
+  cout << "top.Q  = " << (int) top.Q << endl;
+
+  assert(top.Q == ((-8 / 2) & 0xffff));
+
 }
